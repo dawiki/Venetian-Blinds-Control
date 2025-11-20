1afd34328b7fc067d933412fc933e16828af1076
Herwig Guschelbauer <dawiki@me.com>
2025-11-20 10:26:49 +0100

    remove duplicated id specification

--- components/venetian_blinds/cover.py
+++ components/venetian_blinds/cover.py
@@ -14,26 +14,28 @@ from esphome.const import (
 CONF_TILT_DURATION = "tilt_duration"
 CONF_ACTUATOR_ACTIVATION_DURATION = "actuator_activation_duration"
 
-venetian_blinds_ns = cg.esphome_ns.namespace('venetian_blinds')
-VenetianBlinds = venetian_blinds_ns.class_('VenetianBlinds', cover.Cover, cg.Component)
+venetian_blinds_ns = cg.esphome_ns.namespace("venetian_blinds")
+VenetianBlinds = venetian_blinds_ns.class_("VenetianBlinds", cover.Cover, cg.Component)
 
 CONFIG_SCHEMA = (
     cover.cover_schema(VenetianBlinds)
     .extend(
         {
-            cv.GenerateID(): cv.declare_id(VenetianBlinds),
             cv.Required(CONF_OPEN_ACTION): automation.validate_automation(single=True),
             cv.Required(CONF_OPEN_DURATION): cv.positive_time_period_milliseconds,
             cv.Required(CONF_CLOSE_ACTION): automation.validate_automation(single=True),
             cv.Required(CONF_CLOSE_DURATION): cv.positive_time_period_milliseconds,
             cv.Required(CONF_STOP_ACTION): automation.validate_automation(single=True),
             cv.Required(CONF_TILT_DURATION): cv.positive_time_period_milliseconds,
-            cv.Optional(CONF_ACTUATOR_ACTIVATION_DURATION, default="0s"): cv.positive_time_period_milliseconds,
-            cv.Optional(CONF_ASSUMED_STATE, default=True): cv.boolean
+            cv.Optional(
+                CONF_ACTUATOR_ACTIVATION_DURATION, default="0s"
+            ): cv.positive_time_period_milliseconds,
+            cv.Optional(CONF_ASSUMED_STATE, default=True): cv.boolean,
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
 )
+
 
 async def to_code(config):
     var = await cover.new_cover(config)
@@ -53,7 +55,9 @@ async def to_code(config):
     await automation.build_automation(
         var.get_close_trigger(), [], config[CONF_CLOSE_ACTION]
     )
-    
+
     cg.add(var.set_tilt_duration(config[CONF_TILT_DURATION]))
-    cg.add(var.set_actuator_activation_duration(config[CONF_ACTUATOR_ACTIVATION_DURATION]))
+    cg.add(
+        var.set_actuator_activation_duration(config[CONF_ACTUATOR_ACTIVATION_DURATION])
+    )
     cg.add(var.set_assumed_state(config[CONF_ASSUMED_STATE]))
